6c63d05ed4ba82ff29ceec25fbd837fea8910f67
Janayan64 <janayan.aam@obmontessori.edu.ph>
2026-01-20 08:43:35 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from pyscript import display
+
+
+
+def info(e):
+    document.getElementById('info').innerHTML = ' '
+    document.getElementById('image').innerHTML = ' '
+
+    reg = document. querySelector('input[name="online"]:checked').value
+    clear = document.querySelector('input[name="med"]:checked').value
+    gradenum  = int(document.getElementById)('grade'.value)
+    section = document.getElementById('sec').value
+
+    if reg = yeson and clear = yesmed
+        print("You are now in:")
+    else:
+        print("One of the requirements is missing.")
+    
+
+    #if number % 2 == 0; pecentage means divison (remainder)
+
+
+
+    
